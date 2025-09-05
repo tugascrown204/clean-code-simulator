c7fb8370893eb2daab192b11d058e785ec5f9caa
tugascrown204 <gajdertevet@hotmail.com>
2025-09-05 21:46:50 +0330

    feat: initial commit for public/app.js

---
+++ public/app.js
@@ -0,0 +1,4 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const challengesDiv = document.getElementById('challenges');
+    challengesDiv.innerHTML = '<p>Load challenges here...</p>';
+});
